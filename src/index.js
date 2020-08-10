ef8edf105fabd9caa8fdbeb56892c78210f71344
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-10 19:40:06 +0200

    Otro dibujo más.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import lottie from 'lottie-web';
 
 const element = document.getElementById('dibujo');
+const element2 = document.getElementById('dibujo2');
 
 lottie.loadAnimation({
     container: element, // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '29178-cat-in-box.json' // the path to the animation json
+    //path: '29178-cat-in-box.json' // the path to the animation json
+    path: '29056-nepenthe-illustration.json'
+  });
+
+  lottie.loadAnimation({
+    container: element2, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    //path: '29178-cat-in-box.json' // the path to the animation json
+    path: '29178-cat-in-box.json'
   });
