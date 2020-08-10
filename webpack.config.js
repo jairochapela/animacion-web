69ac6cf502b7e7074b52b52627d39df3d77f27f5
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-10 19:09:47 +0200

    Primer commit.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        library: 'Animacion'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+}
